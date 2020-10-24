c28bafd468b2a609902ad3bffe461ae7c2fbeee0
drys262 <koeichavez17@gmail.com>
2020-10-24 15:03:21 +0800

    feat: add import map & deps.ts

--- date-fns.ts
+++ date-fns.ts
@@ -1,4 +1,4 @@
-import { compareAsc, format } from "https://cdn.skypack.dev/date-fns@^2.16.1";
+import { compareAsc, format } from "date-fns";
 
 const formatValue = format(new Date(2014, 1, 11), "yyyy-MM-dd", {});
 //=> '2014-02-11'
---
+++ ramda.ts
@@ -0,0 +1,4 @@
+import { add, multiply } from "./deps.ts";
+
+console.log(add(1, 2));
+console.log(multiply(2, 2));
